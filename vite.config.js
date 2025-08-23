50ad5908fe7c1f28c0c68de2c92620496224b0b3
ash-norsidi <ash-norsidi@example.com>
2025-08-22 20:50:02 -0400

    Update build with fresh assets

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/software-engineer-study-hub/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
