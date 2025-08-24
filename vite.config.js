05b8090d23d1d8491cab20a8029a36c2cc37d9f7
ash-norsidi <ash-norsidi@example.com>
2025-08-24 04:29:32 -0400

    Update Vite config and rebuild for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,19 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/software-engineer-study-hub/',
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
+
+
